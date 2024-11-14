8ffc8f7072831748691f49d943b692e5ea712f24
andreiwulff <104514076+andreiwulff@users.noreply.github.com>
2024-11-14 22:22:03 +0200

    add

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+https://github.com/andreiwulff/Module_4_test/blob/ed20e238c1c6373611ecedf0a8f2cd553878e572/main.py#L16
